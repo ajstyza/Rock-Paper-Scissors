4735f2d5c182d7c4295702ae511a2142c3ab5ee2
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-04-12 11:47:58 -0700

    completed RPS project

---
+++ script.js
@@ -0,0 +1,35 @@
+var wins = 0;
+var loses = 0;
+var ties = 0;
+
+
+var options = ["R", "P", "S"];
+
+var playGame = function (){
+
+var userChoice = window.prompt("Input R, P, or S, for Rock, Paper, or Scissors");
+
+if (!userChoice) {
+    return;
+}
+
+var index = Math.floor(Math.random() * options.length);
+var computerChoice = options[index];
+
+window.alert("the computer chose" + computerChoice);
+
+if(userChoice === computerChoice) {
+    ties++
+}
+// calculate user win scenarios
+
+else if (
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerchoice === "P") 
+) {
+     wins++
+    window.alert("you won");
+    }
+}
+playGame();
